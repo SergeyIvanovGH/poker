801a21f6f81d36f47c7602be017f9dc7fcc7d9f9
Serhii Ivanov <sergey.ivanov@meta.ua>
2016-07-14 11:39:51 +0300

    add: Base authentification compete

--- src/main/java/ua/com/univerpulse/config/SecurityConfiguration.java
+++ src/main/java/ua/com/univerpulse/config/SecurityConfiguration.java
@@ -1,16 +1,23 @@
 package ua.com.univerpulse.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import ua.com.univerpulse.service.UserDetailsServiceImpl;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -27,8 +34,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         };
     }
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
+    @Autowired
+    public void registerGlobalAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public UserDetailsService getUserDetailsService(){
+        return new UserDetailsServiceImpl();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 //            http.authorizeRequests().antMatchers("/").permitAll();
 //            http.csrf().disable();  // Disable cross request
 
@@ -46,15 +63,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 //                    .logout()
 //                    .permitAll();
 
-            http
-                    .httpBasic()
-                    .and()
-                    .authorizeRequests()
-                    .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
-                    .antMatchers("/css/**").permitAll()
-                    .antMatchers("/images/**").permitAll()
-                    .antMatchers("/js/**").permitAll()
-                    .antMatchers("/service/**").permitAll()
-                    .anyRequest().authenticated();
-        }
+        http
+            .httpBasic()
+            .and()
+            .authorizeRequests()
+            .antMatchers("/index.html", "/home.html").permitAll()
+            .antMatchers("/css/**").permitAll()
+            .antMatchers("/images/**").permitAll()
+            .antMatchers("/js/**").permitAll()
+            .antMatchers("/service/**").permitAll()
+            .anyRequest().authenticated();
+    }
 }
--- src/main/java/ua/com/univerpulse/service/UserDetailsServiceImpl.java
+++ src/main/java/ua/com/univerpulse/service/UserDetailsServiceImpl.java
@@ -1,6 +1,9 @@
 package ua.com.univerpulse.service;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,25 +11,29 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ua.com.univerpulse.model.RegistrationRequest;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
-public class UserDetailServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private static final Log logger = LogFactory.getLog(UserDetailsServiceImpl.class);
 
     @Autowired
     private RegistrationService registrationService;
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        System.out.println(s);
+        logger.info("loadUserByUsername called");
 
         Optional<RegistrationRequest> userDetails = registrationService.findByName(s);
         RegistrationRequest registrationRequest = userDetails.orElseThrow(() -> new UsernameNotFoundException(
                 String.format("User with email=%s was not found", s)));
 
-        System.out.println(registrationRequest.geteMail());
-        System.out.println(registrationRequest.getPassword());
+        logger.info(registrationRequest.geteMail());
+        logger.info(registrationRequest.getPassword());
 
-        return new User(registrationRequest.geteMail(), registrationRequest.getPassword(), null);
+        return new User(registrationRequest.geteMail(), registrationRequest.getPassword(), new HashSet<>());
     }
 }
